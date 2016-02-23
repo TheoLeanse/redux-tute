0c92647e56cf81fe20a195757d59c2f55f7a96d9
TheoLeanse <theoleanse@gmail.com>
2016-02-23 07:35:02 +0000

    add reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,12 @@
-export function reducer(state, action) {
-    // figure out which function to call and call it
+import {setEntries, next, vote} from './core';
+
+export default function reducer(state, action) {
+    switch (action.type) {
+    case 'SET_ENTRIES':
+        return setEntries(state, action.entries);
+    case 'NEXT':
+        return next(state);
+    case 'VOTE':
+        return vote(state, action.entry);
+    }
 }
